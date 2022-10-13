780c3748f82969ae22c8d41e487619e7039f6746
Janio-Rosa <janio@ufu.br>
2022-10-13 14:35:32 -0400

    use metamask networkd id insted of local id

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 //import logo from '../logo.png';
-import ethLogo from '../eth-logo.png';
+//import ethLogo from '../eth-logo.png';
 //import tokenLogo from '../token-image.jpg';
-import tokenLogo from '../token-logo.png';
+//import tokenLogo from '../token-logo.png';
 import BuyForm from './BuyForm'
 import SellForm from './SellForm'
 class Main extends Component {
